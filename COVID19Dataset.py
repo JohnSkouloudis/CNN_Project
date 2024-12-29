75fcea2d9244743cf1b5a55ebff807f2d3396b55
John skoul <johnskoul56@gmail.com>
2024-12-29 23:10:40 +0200

    first commit

---
+++ COVID19Dataset.py
@@ -0,0 +1,12 @@
+import torch
+import random
+from torch.utils.data import Dataset
+
+class COVID19Dataset(Dataset):
+
+    def __init__(self):
+
+    def _getitem(self):
+
+    def display_batch(self,indexes):
+
